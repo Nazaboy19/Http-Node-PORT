8b74550696da996b1180387e903bfd6ce81bb1c4
Ibrahim Mercado <Nazaboy19@Ibrahims-MacBook-Pro.local>
2016-06-21 19:48:31 -0400

    double ports

--- jun21cw2app.js
+++ jun21cw2app.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 var PORT = 7500;
 
 var server = http.createServer(function(req,res) {
@@ -9,9 +9,9 @@ var server = http.createServer(function(req,res) {
 	});
 
 
-	var body = '<u><b>You Smell!</b> </u>';
+	var body ='<u><b>Whether you think you can or you think you cant, youre right.</b> </u>';
 
-res.end('body');
+res.end(body);
 
 
 });
@@ -20,3 +20,40 @@ res.end('body');
 server.listen(PORT, function() {
 	console.log('Server Listening on %d ', PORT);
 });
+
+*/
+var http = require('http');
+
+// array of insults
+var bad = [
+  'You stuck up, half-witted, scruffy-looking Nerf herder', 
+  'You slimy piece of worm-ridden filth!', 
+  'You flea-bitten furball'
+];
+
+// array of good things
+var good = [
+  'You\'re awesome!', 
+  'I like your style!', 
+  'You are the most perfect you there is.'
+];
+
+http.createServer(handleRequest).listen(7000);
+http.createServer(handleRequest).listen(7500);
+
+function handleRequest(req, res) {
+  console.log(req.url);
+
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+
+  var reply;
+  if (req.socket.localPort == 7000) {
+    reply = good[Math.floor(Math.random() * good.length)];
+  } else {
+    reply = bad[Math.floor(Math.random() * bad.length)];
+  }
+
+  res.end(reply);
+}
--- jun21cwapp.js
+++ jun21cwapp.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/* var http = require('http');
 var PORT = 7000;
 
 var server = http.createServer(function(req,res) {
@@ -11,7 +11,7 @@ var server = http.createServer(function(req,res) {
 
 	var body = '<u><b>You Smell GOOD!</b> </u>';
 
-res.end('body');
+res.end(body);
 
 
 });
@@ -20,3 +20,39 @@ res.end('body');
 server.listen(PORT, function() {
 	console.log('Server Listening on %d ', PORT);
 });
+*/
+var http = require('http');
+
+// array of insults
+var bad = [
+  'You stuck up, half-witted, scruffy-looking Nerf herder', 
+  'You slimy piece of worm-ridden filth!', 
+  'You flea-bitten furball'
+];
+
+// array of good things
+var good = [
+  'You\'re awesome!', 
+  'I like your style!', 
+  'You are the most perfect you there is.'
+];
+
+http.createServer(handleRequest).listen(7000);
+http.createServer(handleRequest).listen(7500);
+
+function handleRequest(req, res) {
+  console.log(req.url);
+
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+
+  var reply;
+  if (req.socket.localPort == 7000) {
+    reply = good[Math.floor(Math.random() * good.length)];
+  } else {
+    reply = bad[Math.floor(Math.random() * bad.length)];
+  }
+
+  res.end(reply);
+}
